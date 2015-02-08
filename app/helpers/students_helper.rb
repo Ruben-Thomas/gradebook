409896bed22067a4ce9673872d07e640008cc904
Thomas Midgett <tmidge20@gmail.com>
2015-02-07 21:16:13 -0500

    Adds helpers

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,10 @@
 module StudentsHelper
+  def student_grades(student)
+    result = "<table>"
+    student.grades.each do |grade|
+      result <<"<p>Grade: #{grade.grade} Date of assignment: #{grade.date}</p>"
+    end
+    result << "</table>"
+    result.html_safe
+  end
 end
